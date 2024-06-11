e49d653a605610afce6f8c38d869c65a3e9f8f7b
gudkxv <fafik23smurf@mai.ru>
2024-06-11 18:53:39 +0300

    Created blank project with default gradle CI. Updated README.md

---
+++ src/main/java/gudkov/miit/gudkovmiddleservice/GudkovMiddleServiceApplication.java
@@ -0,0 +1,13 @@
+package gudkov.miit.gudkovmiddleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GudkovMiddleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GudkovMiddleServiceApplication.class, args);
+    }
+
+}
